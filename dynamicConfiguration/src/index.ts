e1eb934f793a2b7834d0812a5b7a0ec1079e8466
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-12 22:27:10 +0300

    Add docker compose config

--- dynamicConfiguration/src/index.ts
+++ dynamicConfiguration/src/index.ts
@@ -1,9 +1,21 @@
+import mongoose from "mongoose";
 import { app } from "./app";
 
-const checkEnvironmentVars = () => {};
+const checkEnvironmentVars = () => {
+    if (process.env.MONGO_URI === undefined) {
+        throw new Error("MONGO_URI is not defined");
+    }
+    if (process.env.PORT === undefined) {
+        throw new Error("PORT is not defined");
+    }
+};
 
 const start = async () => {
     checkEnvironmentVars();
+    mongoose
+        .connect(process.env.MONGO_URI!)
+        .then(r => console.log(r.connection.db.databaseName))
+        .catch(e => console.log(e));
     app.listen(process.env.PORT || 5000, () => {
         console.log(`listening on ${process.env.PORT || 5000}`);
     });
