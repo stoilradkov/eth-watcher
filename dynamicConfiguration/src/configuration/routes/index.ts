b991611c371a80f037d6a2875bd92c432a54806c
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-13 10:31:38 +0300

    Add delete endpoint

--- dynamicConfiguration/src/configuration/routes/index.ts
+++ dynamicConfiguration/src/configuration/routes/index.ts
@@ -2,5 +2,12 @@ import { getConfigurationByIdHandler } from "./getConfigurationById";
 import { getConfigurationsHandler } from "./getConfigurations";
 import { postConfigurationHandler } from "./postConfiguration";
 import { putConfigurationHandler } from "./putConfiguration";
+import { deleteConfigurationHandler } from "./deleteConfiguration";
 
-export { getConfigurationByIdHandler, getConfigurationsHandler, postConfigurationHandler, putConfigurationHandler };
+export {
+    getConfigurationByIdHandler,
+    getConfigurationsHandler,
+    postConfigurationHandler,
+    putConfigurationHandler,
+    deleteConfigurationHandler,
+};
