f181130c62c5a5689ed367e73ebf20a236616aa6
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-15 09:23:28 +0300

    Add error handling

--- transactionWatcher/src/store/saveTransactions.ts
+++ transactionWatcher/src/store/saveTransactions.ts
@@ -1,11 +1,22 @@
 import { Model, ModelStatic } from "sequelize/types";
+import { logError } from "../logger";
 import { TransactionAttributes } from "./models/Transaction.type";
 
+/**
+ * Returns a function which creates and saves an array of transactions in the store
+ * @param transactionModel - instance of transaction model
+ * @returns a function which creates and saves an array of transactions in the store
+ */
 export const getSaveTransactions = (
     transactionModel: ModelStatic<Model<TransactionAttributes, TransactionAttributes>>
 ) => {
     return async (transactionsPayload: TransactionAttributes[]) => {
-        await transactionModel.bulkCreate(transactionsPayload);
-        return true;
+        try {
+            await transactionModel.bulkCreate(transactionsPayload);
+            return true;
+        } catch (e) {
+            logError("Could not save transactions", transactionModel, transactionsPayload, e);
+        }
+        return false;
     };
 };
